0da0f2ad23b83a527457b8be2a0485374e9d4676
Dominik Schmidt <dev@dominik-schmidt.de>
2012-04-30 20:53:16 +0200

    clucene: update to 2.3.3.4, upstream it after testing

---
+++ clucene.rb
@@ -0,0 +1,22 @@
+require 'formula'
+
+class Clucene < Formula
+  homepage 'http://sourceforge.net/projects/clucene/'
+  url 'http://downloads.sourceforge.net/project/clucene/clucene-core-unstable/2.3/clucene-core-2.3.3.4.tar.gz'
+  md5 '48d647fbd8ef8889e5a7f422c1bfda94'
+  head 'git://clucene.git.sourceforge.net/gitroot/clucene/clucene'
+
+  def install
+#     if ARGV.build_head?
+      system "cmake #{std_cmake_parameters} ."
+#     else
+#       system "./configure", "--disable-debug", "--disable-dependency-tracking",
+#                             "--prefix=#{prefix}"
+#     end
+
+    # Serialize the install step. See:
+    # https://github.com/mxcl/homebrew/issues/8712
+    ENV.j1
+    system "make install"
+  end
+end
