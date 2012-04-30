cb833d6c39ab40cfbe8a1848d978b9cc53f4fa12
Dominik Schmidt <dev@dominik-schmidt.de>
2012-04-30 23:48:45 +0200

    tomahawk: add formula. this is broken, currently the tomahawk build system can only build bundles on osx. this formula aggregates all tomahawk deps"

---
+++ tomahawk.rb
@@ -0,0 +1,51 @@
+require 'formula'
+
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Tomahawk < Formula
+  homepage ''
+  url 'http://downloads.tomahawk-player.org/tomahawk-0.4.2.tar.bz2'
+  head 'git://github.com/tomahawk-player/tomahawk.git'
+  md5 '2c5aadf3c6a3a9b8b7c66ca29bbf88fb'
+
+  # required:
+  depends_on 'cmake' => :build
+  depends_on 'pkgconfig' => :build
+  depends_on 'qt'
+  depends_on 'qjson'
+  depends_on 'taglib'
+  depends_on 'boost'
+  depends_on 'libechonest'
+  depends_on 'kde-phonon'
+  depends_on 'quazip'
+  depends_on 'attica'
+
+  # upstream this formula
+  depends_on 'tomahawk-player/tomahawk/clucene'
+
+  # FIXME
+  # depends_on 'phonon-vlc' # vlc.rb is currently broken
+
+  # optional:
+  depends_on 'qtweetlib'
+  depends_on 'jreen'
+  depends_on 'qca'
+  depends_on 'qca-ossl'
+
+
+
+
+  def install
+    system "cmake . #{std_cmake_parameters}"
+    system "make install" # if this fails, try separate make/make install steps
+    system "exit"
+  end
+
+  def test
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test tomahawk`.
+    system "false"
+  end
+end
