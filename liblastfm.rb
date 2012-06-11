30d857d28a2d6aa747429cce85ee441efad4ae2b
Christian Muehlhaeuser <muesli@gmail.com>
2012-06-11 22:13:45 +0200

    * Fixed lastfm formula.

--- liblastfm.rb
+++ liblastfm.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class liblastfm < Formula
+class Liblastfm < Formula
   head 'git://github.com/eartle/liblastfm.git'
   url 'http://users.last.fm/~michael/client/liblastfm-1.0.0.tar.gz'
   md5 '2cf7a63da14cbfe96160fbaf41c1bdd5'
