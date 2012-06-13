44819940bd91c1928e16f93412c96aa99d8b828d
Leo Franchi <lfranchi@kde.org>
2012-06-13 13:49:18 +0200

    Add qt dep to jreen

--- jreen.rb
+++ jreen.rb
@@ -5,6 +5,7 @@ class Jreen < Formula
   url 'http://qutim.org/dwnl/39/libjreen-1.1.0.tar.bz2'
   md5 '84d483d59976fcbaa7951dd0acfa689a'
 
+  depends_on 'qt'
   depends_on 'cmake'
   depends_on 'qca'
   depends_on 'qca-ossl'
