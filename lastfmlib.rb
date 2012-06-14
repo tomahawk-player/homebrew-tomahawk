878d90ec4ca851ff37d51c70d018205e8a7f162f
Leo Franchi <lfranchi@kde.org>
2012-06-14 21:56:58 +0200

    Fix POST'ing with qt 4.8
    Upstream pull request for post-1.0: https://github.com/eartle/liblastfm/pull/9

--- lastfmlib.rb
+++ lastfmlib.rb
@@ -8,6 +8,10 @@ class Lastfmlib < Formula
   depends_on 'cmake'
   depends_on 'qt'
 
+  def patches
+    'https://raw.github.com/gist/2932406/b8356d788d750f1580dc34a78177b0436c806802/liblastfm-4.8-POST-fix'
+  end
+
   def install
     system "cmake . #{std_cmake_parameters}"
     system "make install"
