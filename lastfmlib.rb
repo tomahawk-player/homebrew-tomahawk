3fbd2cb2f779538bc96934588fb30ca90d0261a0
Christian Muehlhaeuser <muesli@gmail.com>
2012-06-20 14:22:38 +0300

    Updated lastfmlib to 1.0.1.

--- lastfmlib.rb
+++ lastfmlib.rb
@@ -2,15 +2,11 @@ require 'formula'
 
 class Lastfmlib < Formula
   head 'git://github.com/eartle/liblastfm.git'
-  url 'http://cdn.last.fm/client/liblastfm-1.0.0.tar.gz'
-  md5 '2cf7a63da14cbfe96160fbaf41c1bdd5'
+  url 'http://cdn.last.fm/client/liblastfm-1.0.1.tar.gz'
+  md5 '77fb72721e6e027d73110a8f66191cca'
 
   depends_on 'cmake'
   depends_on 'qt'
-
-  def patches
-    'https://raw.github.com/gist/2932406/b8356d788d750f1580dc34a78177b0436c806802/liblastfm-4.8-POST-fix'
-  end
 
   def install
     system "cmake . #{std_cmake_parameters}"
