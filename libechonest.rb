dcea471bfed45bf7d5099e80c161cef07865c333
Leo Franchi <lfranchi@kde.org>
2012-07-22 11:27:41 -0400

    Bump to 2.0.1

--- libechonest.rb
+++ libechonest.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libechonest < Formula
   homepage 'https://projects.kde.org/projects/playground/libs/libechonest'
-  url 'http://files.lfranchi.com/libechonest-2.0.0.tar.bz2'
-  sha1 '817a7c2b821d23edbbf6c387deefbfc6dddee66c'
+  url 'http://files.lfranchi.com/libechonest-2.0.1.tar.bz2'
+  sha1 '5dd98ffb370e0e199e37ece4a1775a05594f3dcb'
 
   depends_on 'cmake' => :build
   depends_on 'qt'
