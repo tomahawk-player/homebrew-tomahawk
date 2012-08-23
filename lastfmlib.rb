7d75e7592b42ab0a30e70a7201b438fea1623851
Dominik Schmidt <dev@dominik-schmidt.de>
2012-08-23 17:40:39 +0300

    lastfmlib.rb: update head url

--- lastfmlib.rb
+++ lastfmlib.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Lastfmlib < Formula
-  head 'git://github.com/eartle/liblastfm.git'
+  head 'git://github.com/lastfm/liblastfm.git'
   url 'http://cdn.last.fm/client/liblastfm-1.0.1.tar.gz'
   md5 '77fb72721e6e027d73110a8f66191cca'
 
