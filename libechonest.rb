95cf448c3cfa198c23a9a85fd04eb22becf875c9
Christian Muehlhaeuser <muesli@gmail.com>
2013-01-10 01:00:43 +0100

    Updated libechonest recipe to 2.0.2.

--- libechonest.rb
+++ libechonest.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libechonest < Formula
   homepage 'https://projects.kde.org/projects/playground/libs/libechonest'
-  url 'http://files.lfranchi.com/libechonest-2.0.1.tar.bz2'
-  sha1 '5dd98ffb370e0e199e37ece4a1775a05594f3dcb'
+  url 'http://files.lfranchi.com/libechonest-2.0.2.tar.bz2'
+  sha1 '346eba6037ff544f84505941832604668c1e5b2b'
 
   depends_on 'tomahawk-player/tomahawk/cmake' => :build
   depends_on 'qt'
