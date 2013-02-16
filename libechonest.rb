e432f42a9d57b879bd8a39f73a8e5677058c9fd6
Leo Franchi <lfranchi@kde.org>
2013-02-16 10:23:46 -0500

    Update libechonest sha1

--- libechonest.rb
+++ libechonest.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Libechonest < Formula
   homepage 'https://projects.kde.org/projects/playground/libs/libechonest'
   url 'http://files.lfranchi.com/libechonest-2.0.3.tar.bz2'
-  sha1 '2fbcfbb9107c0538ba7dd7361e2049cc272d09c8'
+  sha1 '10ada8aced6dce3c0d206afcfbd4b05313bd4d04'
 
   depends_on 'tomahawk-player/tomahawk/cmake' => :build
   depends_on 'qt'
