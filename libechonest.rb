6468c233d922e13a24487a36bbb2487368271dbb
Christian Muehlhaeuser <muesli@gmail.com>
2013-04-09 16:13:13 +0300

    Comment out conflicting cmake dependency. (conflicts with qjson)

--- libechonest.rb
+++ libechonest.rb
@@ -5,7 +5,7 @@ class Libechonest < Formula
   url 'http://files.lfranchi.com/libechonest-2.0.3.tar.bz2'
   sha1 '10ada8aced6dce3c0d206afcfbd4b05313bd4d04'
 
-  depends_on 'tomahawk-player/tomahawk/cmake' => :build
+  #depends_on 'tomahawk-player/tomahawk/cmake' => :build
   depends_on 'qt'
   depends_on 'qjson'
 
