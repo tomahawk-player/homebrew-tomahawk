48579f02922808c231e6a50ea15f6f51947911f9
Christian Muehlhaeuser <muesli@gmail.com>
2014-01-25 00:56:26 +0100

    * Bump Phonon to 4.7.1 and phonon-vlc to 0.7.1.

--- phonon-vlc.rb
+++ phonon-vlc.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class PhononVlc <Formula
   homepage 'https://projects.kde.org/projects/kdesupport/phonon/phonon-vlc'
-  url "http://mirrors.mit.edu/kde/stable/phonon/phonon-backend-vlc/0.6.1/src/phonon-backend-vlc-0.6.1.tar.xz"
-  md5 "d227b92619124a2b85e2d2e5f0fff90e"
+  url "http://mirrors.mit.edu/kde/stable/phonon/phonon-backend-vlc/0.7.1/phonon-backend-vlc-0.7.1.tar.xz"
+#  md5 "d227b92619124a2b85e2d2e5f0fff90e"
 
   head 'git://anongit.kde.org/phonon-vlc'
 
@@ -29,12 +29,5 @@ class PhononVlc <Formula
     print "pluginDir: #{pluginDir} #{Formula.factory('qt').prefix}"
     system "rm -f #{pluginDir}/*"
     system "cp #{prefix}/lib/kde4/plugins/phonon_backend/phonon_vlc.so #{pluginDir}"
-  end
-
-  def patches
-    # Turn of an additional phonon option
-    return [ "https://gist.github.com/raw/4380358/16a7252c04c603e9d3c9a64d8b9c4c3adf97798e/phononvlc-check-bundle-patch.diff",
-             "https://gist.github.com/lfranchi/2916817/raw/4bc649a2acd2eba1f15d72e0d77a36012ed50080/phonon-vlc-set-plugin-path.patch",
-             "https://gist.github.com/lfranchi/4105819/raw/670e93027417ec36ff730ada6abbc2e8b305c3ae/phonon-vlc-optimize-debug" ]
   end
 end
