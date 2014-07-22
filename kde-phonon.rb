fa42713370135626d059a24220973092c7eb6f7f
Christian Muehlhaeuser <muesli@gmail.com>
2014-07-22 17:13:29 +0200

    Updated sha1.

--- kde-phonon.rb
+++ kde-phonon.rb
@@ -3,7 +3,7 @@ require 'formula'
 class KdePhonon < Formula
   url 'ftp://ftp.kde.org/pub/kde/stable/phonon/4.7.2/phonon-4.7.2.tar.xz'
   homepage 'http://phonon.kde.org/'
-#  md5 'bbe0c1c62ed14c31479c4c1a6cf1e173'
+  sha1 '2b98fa96cefbbef53d77dd0e6d830889da367c37'
   head 'git://anongit.kde.org/phonon.git'
 
   depends_on 'cmake' => :build
