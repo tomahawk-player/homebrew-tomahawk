1fccab820765dae861963d555edc3cdeb524bfb8
Christian Muehlhaeuser <muesli@gmail.com>
2014-07-22 18:00:24 +0200

    Updated sha1.

--- phonon-vlc.rb
+++ phonon-vlc.rb
@@ -3,7 +3,7 @@ require 'formula'
 class PhononVlc <Formula
   homepage 'https://projects.kde.org/projects/kdesupport/phonon/phonon-vlc'
   url "http://mirrors.mit.edu/kde/stable/phonon/phonon-backend-vlc/0.7.2/src/phonon-backend-vlc-0.7.2.tar.xz"
-#  md5 "d227b92619124a2b85e2d2e5f0fff90e"
+  sha1 '635cd0232bd9df4a8a812c569bcf905f242671c3'
 
   head 'git://anongit.kde.org/phonon-vlc'
 
