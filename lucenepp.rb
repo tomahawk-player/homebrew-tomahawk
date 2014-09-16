ce1748712c69c57b04d27b716e8a8061a0190e76
Christian Muehlhaeuser <muesli@gmail.com>
2014-09-16 05:04:05 +0200

    Stable lucenepp version is no good.

--- lucenepp.rb
+++ lucenepp.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Lucenepp < Formula
   homepage 'https://github.com/luceneplusplus/LucenePlusPlus'
-  url 'https://github.com/luceneplusplus/LucenePlusPlus/archive/rel_3.0.6.tar.gz'
+  # url 'https://github.com/luceneplusplus/LucenePlusPlus/archive/rel_3.0.6.tar.gz'
   head 'https://github.com/luceneplusplus/LucenePlusPlus.git'
   
   depends_on 'cmake' => :build
