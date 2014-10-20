b492e0ccf581da981c3faba1d5c13445744f6199
Uwe L. Korn <uwelk@xhochy.com>
2014-10-20 16:06:03 +0200

    [lucenepp] Remove trailing whitespace

--- lucenepp.rb
+++ lucenepp.rb
@@ -4,7 +4,7 @@ class Lucenepp < Formula
   homepage 'https://github.com/luceneplusplus/LucenePlusPlus'
   # url 'https://github.com/luceneplusplus/LucenePlusPlus/archive/rel_3.0.6.tar.gz'
   head 'https://github.com/luceneplusplus/LucenePlusPlus.git'
-  
+
   depends_on 'cmake' => :build
   depends_on 'boost'
 
